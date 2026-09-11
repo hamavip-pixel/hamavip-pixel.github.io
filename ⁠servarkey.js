bfd6aeb3971f00ad9ba8160f583a48567b8e5d93
hamavip-pixel <gorahawrami2@gmail.com>
2026-09-12 01:10:57 +0300

    Create ⁠servarkey.js

---
+++ ⁠servarkey.js
@@ -0,0 +1,13 @@
+// servarkey.js - فایلی تایبەتی کلیلەکان (false واتە بەکارنەهاتووە)
+const validKeys = {
+    "HX-PRO-J9XIX": false,
+    "HX-PRO-K8L2M": false,
+    "HX-PRO-P4W7Q": false,
+    "HX-PRO-Z9X1V": false,
+    "HX-PRO-B3N6C": false,
+    "HX-PRO-H5F8D": false,
+    "HX-PRO-R2T9Y": false,
+    "HX-PRO-L7K4J": false,
+    "HX-PRO-X1M3N": false,
+    "HX-PRO-V6C5B": false
+};
